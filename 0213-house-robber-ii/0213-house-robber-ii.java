115e025b0ed851d671329341cb589ddda871ec2d
Sunny nagar <sunnynagar3134@gmail.com>
2026-07-24 23:23:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (42.15%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(int[] nums) {
+       
+       int n = nums.length;
+
+       if(n == 1) return nums[0];
+
+       return Math.max(robHelper(nums,0,n-2),robHelper(nums,1,n-1));
+    }
+
+    int robHelper(int[] nums,int start,int end){
+
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i=start;i<=end;i++){
+
+            int pick = nums[i] + prev2;
+
+            int notPick = prev1;
+
+            int curr = Math.max(pick,notPick);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
